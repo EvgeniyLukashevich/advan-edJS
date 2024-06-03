24ce499dbbb8276a045c1c990690c42544ec1405
Evgeniy Phive <phivedec@gmail.com>
2024-06-03 23:47:06 +0800

    the 3rd homework added

--- seminar3/static/js/modules/utils.js
+++
@@ -1,7 +0,0 @@
-import { localStorageManager } from "./localStorageManager.js";
-
-function getAllReviews() {
-    const reviews = localStorageManager.getItem(productReviews);
-    return reviews ? reviews : {};
-}
-
